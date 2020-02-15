e47fe0b9b71424a3b7bb7caaa0ad8624d7fddab8
unknown <rohanjyapumaharjan@gmail.com>
2020-02-15 08:19:49 +0545

    Aded a  file

---
+++ Rohan/1st.py
@@ -0,0 +1,59 @@
+# data types are the building blocks upon which larger program are made.
+# integer
+# int Rohanmaharjan - PASCAL CASE (doesnt work in python)
+# int rohanMaharjan - SNAKE CASE (doesnt work in python)
+# int rohan_maharjan
+a=4+5
+print(a)
+
+b=4-5
+print(b)
+
+c=4**2
+print(c)
+
+print("Hello world!\n" * 150) 
+
+d=4**0.5
+print(d)
+
+e=4/3
+print(e)
+
+print(4%3)
+
+print(0.1+0.2-0.3)
+
+# Mutability/immutability
+
+# indexing
+a = "hello"
+print(a[-1])
+
+# slicing
+# [start:stop:step]
+a = "Hello"
+print(a[0:3])
+print(a[ : ])
+
+# indexing
+any_word="Hello World"
+print(any_word[3])
+
+print(any_word[-1])
+print(any_word[-2])
+print(any_word[-3])
+print(any_word[-4])
+
+# slicing
+print(any_word[ : ])
+
+print(any_word[1: ])
+
+print(any_word[0:11:1])
+
+print(any_word[0:11:2])
+
+print(any_word[ : :-1]) #reverse
+
+
