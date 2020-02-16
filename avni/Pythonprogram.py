b28b3fe689bf827ac6aeb494f1c8f589f7aee99c
Avni8 <avnighimire2000@gmail.com>
2020-02-16 16:31:16 +0545

    file added

---
+++ avni/Pythonprogram.py
@@ -0,0 +1,545 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Datatypes are the building blocks upon which larger programs are made.
+
+# Integer
+
+# In[ ]:
+
+
+int asd=5 - Normal declaration
+
+
+# In[ ]:
+
+
+int Datastructure - PASCAL CASE
+
+
+# In[ ]:
+
+
+int dataStructure - SNAKE CASE
+
+
+# In[ ]:
+
+
+int data_structure
+
+
+# In[1]:
+
+
+4+5
+
+
+# In[2]:
+
+
+4-5
+
+
+# In[3]:
+
+
+4/5
+
+
+# In[4]:
+
+
+4 ** 2
+
+
+# In[5]:
+
+
+print("hello world"*150)
+
+
+# In[6]:
+
+
+4 ** 0.5
+
+
+# In[7]:
+
+
+4/3
+
+
+# 4%3 -MOD
+
+# In[8]:
+
+
+1+2*3
+
+
+# In[9]:
+
+
+(1+2)*3
+
+
+# In[10]:
+
+
+4/3
+
+
+# In[11]:
+
+
+0.1+0.2-0.3
+
+
+# In[13]:
+
+
+Maze=5
+
+
+# In[12]:
+
+
+Maze="Hello"
+
+
+# In[14]:
+
+
+0.1+0.3-0.4
+
+
+# In[15]:
+
+
+any_word="Hello World"
+
+
+# In[16]:
+
+
+any_word[0]
+
+
+# In[17]:
+
+
+any_word[-1]
+
+
+# In[18]:
+
+
+any_word[6]
+
+
+# In[19]:
+
+
+any_word
+
+
+# In[20]:
+
+
+any_word[0:5]
+
+
+# In[21]:
+
+
+any_word[:]
+
+
+# In[22]:
+
+
+any_word[:5]
+
+
+# In[23]:
+
+
+any_word[1:]
+
+
+# In[24]:
+
+
+any_word[0:14:1]
+
+
+# In[25]:
+
+
+any_word[: :-1]
+
+
+# List
+# List is an ordered sequence of elements upon which array like operations can be performed.
+
+# In[26]:
+
+
+a="sam"
+
+
+# In[27]:
+
+
+a[0]
+
+
+# In[28]:
+
+
+a[0]='p'
+
+
+# In[29]:
+
+
+a="pam"
+
+
+# In[30]:
+
+
+a=[4,4.5,"hello"]
+
+
+# In[31]:
+
+
+a[0]
+
+
+# In[32]:
+
+
+a=[1,2,"Hi"]
+
+
+# In[33]:
+
+
+a="Hello world"
+
+
+# In[34]:
+
+
+a.upper()
+
+
+# In[35]:
+
+
+a.lower()
+
+
+# In[36]:
+
+
+a="Hello World"
+
+
+# In[37]:
+
+
+type(a)
+
+
+# In[38]:
+
+
+a.split()
+
+
+# In[40]:
+
+
+type(a.split())
+
+
+# In[41]:
+
+
+a="HeraPheri"
+
+
+# In[42]:
+
+
+a.split('e')
+
+
+# In[43]:
+
+
+'1'+'1'
+
+
+# In[44]:
+
+
+1+1
+
+
+# In[45]:
+
+
+"hello"+"world"
+
+
+# In[46]:
+
+
+1+'1'
+
+
+# In[66]:
+
+
+a=[1,1.1,"Hello"]
+
+
+# In[67]:
+
+
+a[2]
+
+
+# In[62]:
+
+
+a="Hello \n World"
+
+
+# In[63]:
+
+
+print(a)
+
+
+# In[68]:
+
+
+a=[1,1.1,["hello","world"]]
+
+
+# In[69]:
+
+
+a[2]
+
+
+# In[70]:
+
+
+a[2][1]
+
+
+# In[71]:
+
+
+a=[3,1,5,3,4,5,10]
+
+
+# In[72]:
+
+
+a.sort()
+
+
+# In[73]:
+
+
+print(a)
+
+
+# In[77]:
+
+
+a=['e','f','a','b']
+
+
+# In[78]:
+
+
+a.sort()
+
+
+# In[80]:
+
+
+print(a)
+
+
+# Dictionary
+# Unordered pair arrangement
+
+# In[81]:
+
+
+a={'key': 'value'}
+
+
+# In[82]:
+
+
+a={'rollnumber':1,'name':'Aanchal','gender':'Female'}
+
+
+# In[83]:
+
+
+a[0]
+
+
+# In[84]:
+
+
+a['rollnumber']
+
+
+# In[85]:
+
+
+a['name']
+
+
+# In[86]:
+
+
+a['gender']
+
+
+# Tuple(1,1,1,'Hello World')
+
+# In[87]:
+
+
+a=[1,1.1,'Hello World']
+
+
+# In[88]:
+
+
+a[0]
+
+
+# In[93]:
+
+
+a[2]
+
+
+# In[97]:
+
+
+list=[1,1.1,'Hello World']
+
+
+# In[96]:
+
+
+tuple=(1,1.1,'hello world')
+
+
+# In[98]:
+
+
+list[0]
+
+
+# In[99]:
+
+
+list[0]=2
+
+
+# In[100]:
+
+
+list
+
+
+# In[101]:
+
+
+tuple[0]
+
+
+# In[102]:
+
+
+tuple[0]=2
+
+
+# In[103]:
+
+
+a=[1,1.1,"Hello"]
+
+
+# In[104]:
+
+
+a.pop(0)
+
+
+# In[105]:
+
+
+a
+
+
+# In[106]:
+
+
+a.append(2)
+
+
+# In[107]:
+
+
+a
+
+
+# Set:
+#     should always contain unique value
+
+# In[108]:
+
+
+a=[1,2,3,1,1,2,4,5,2]
+
+
+# In[109]:
+
+
+set(a)
+
+
+# Simple I/O operations
+
+# In[119]:
+
+
+get_ipython().run_line_magic('', 'writefile lesson.txt')
+   This is my first python lesson
+    I am learning python
+
+
+# In[ ]:
+
+
+
+
